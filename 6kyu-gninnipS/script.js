b2708aedcccc34ecb8646d42ea9ef43e70f530c7
leejihoon <ljhljh211@naver.com>
2022-06-22 22:26:04 +0900

    [ADD] 6급 spinWords 실습

---
+++ 6kyu-gninnipS/script.js
@@ -0,0 +1,36 @@
+/* 
+Write a function that takes in a string of one or more words, and returns the same string, 
+but with all five or more letter words reversed(Just like the name of this Kata).
+Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
+
+Examples: spinWords("Hey fellow warriors") => returns "Hey wollef sroirraw"
+spinWords("This is a test") => returns "This is a test"
+spinWords("This is another test") => returns "This is rehtona test"
+
+*/
+
+
+
+function spinWords(string) {
+  //TODO Have fun :)
+
+  let arr = string.split(' ');
+  
+  let resultarr = arr.map((v) => {
+    if(v.length > 4) {
+      let spinArr = [];
+      v.split('').forEach((v, i, arr) => {
+        spinArr.push(arr[arr.length - 1 - i])
+      })
+      return spinArr.join('')
+    } else {
+      return v
+    }
+  })
+
+  return resultarr.join(' ');
+}
+
+console.log(
+  spinWords("Welcome hey")
+)
