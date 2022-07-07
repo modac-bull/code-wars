69480722dbf10fbe43f93f313c949947a6d9882e
jihoon <ljhljh211@naver.com>
2022-07-07 17:07:34 +0900

    [MOD] 5kyu beeramid 풀이 과정 주석 추가

--- 5kyu-beeramid/script.js
+++ 5kyu-beeramid/script.js
@@ -19,6 +19,21 @@ beeramid(1500, 2); // should === 12
 beeramid(5000, 3); // should === 16
 
 
+
+요약
+->
+
+input
+보너스 수당 % 맥주 가각 (병당) -> 총 살수 있는 병 갯수
+
+output 
+만들 수 있는 피라미드 갯수
+1^2 + 2^2 + 3^3 + ... + 9^2 + n^2 = 피라미드(n층)을 만들 수 있는 총 병 갯수 
+
+피라미드(n층)을 만들 수 있는 총 병 갯수 < 총 구매 가능한 병의 갯수 < 피라미드(n+1층)을 만들 수 있는 총 병 갯수
+
+-> 결과는 n
+
 */
 // Returns number of complete beeramid levels
 var beeramid = function(bonus, price) {
