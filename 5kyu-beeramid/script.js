734a028bde75033a68bf1943f5f132c50a7bccbb
jihoon <ljhljh211@naver.com>
2022-07-08 14:34:28 +0900

    [MOD] 5kyu beeramid 풀이과정 추가

--- 5kyu-beeramid/script.js
+++ 5kyu-beeramid/script.js
@@ -33,19 +33,29 @@ output
 피라미드(n층)을 만들 수 있는 총 병 갯수 < 총 구매 가능한 병의 갯수 < 피라미드(n+1층)을 만들 수 있는 총 병 갯수
 
 -> 결과는 n
+1, 5, 14, 30, 55, 91 ...
+
+
 
 */
 // Returns number of complete beeramid levels
 var beeramid = function(bonus, price) {
 
+  let maxCnt = Math.floor(bonus/price);
 
   function pyramidTotalCnt(n) {
     let result = 0;
     for( i=0; i<n; i++) {
       result += (i + 1)**2
     }
-    return result
+    return [n, result]
   }
+
+
+  // 1. 1회부터 천천히 돌리는데, pyramidTotaclCnt(n) 보다 커지는 시점에서 n-1 값을 반환
+
+  
+  
   
   console.log(
     pyramidTotalCnt(1),
