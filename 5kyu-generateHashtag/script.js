63435c241fdff4cea625d5c1ee5b64dd33c66c8d
jihoon <ljhljh211@naver.com>
2022-07-12 16:45:56 +0900

    [ADD] 5kyu-generateHashtag 실습 파일 추가

---
+++ 5kyu-generateHashtag/script.js
@@ -0,0 +1,29 @@
+
+/* 
+The marketing team is spending way too much time typing in hashtags.
+Let's help them with our own Hashtag Generator!
+
+Here's the deal:
+
+It must start with a hashtag (#).
+All words must have their first letter capitalized.
+If the final result is longer than 140 chars it must return false.
+If the input or the result is an empty string it must return false.
+Examples
+" Hello there thanks for trying my Kata"  =>  "#HelloThereThanksForTryingMyKata"
+"    Hello     World   "                  =>  "#HelloWorld"
+""                                        =>  false
+*/
+
+function generateHashtag (str) {
+}
+
+
+console.log(
+  generateHashtag(""),
+  generateHashtag("Do We have A Hashtag"),
+  generateHashtag("Codewars"),
+  generateHashtag("Codewars Is Nice"),
+  generateHashtag("Codewars is nice"),
+  generateHashtag("Looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong Cat")
+)
