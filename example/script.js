c92e1a6346a96b1cbbb0258ba992de6324ea7a10
jihoon <ljhljh211@naver.com>
2022-08-23 07:52:03 +0900

    [MOD] 템플릿 수정

--- example/script.js
+++ example/script.js
@@ -1,48 +1,13 @@
-document.addEventListener('DOMContentLoaded', function() {
+/* 
+설명 : 
 
-  let section = document.querySelector('.txt');
 
-
-  // console.log('script!')
-  // myArray = ['abc', 'def', 'wdf', '124', 'asdf']
-  
-  // myArray.shift();
-  // myArray.shift();
-  // myArray.unshift('1256')
-
-  
-  // console.log(section);
-  
-  // section.innerHTML = ' ';
-  // let para1 = document.createElement('p');
-  // para1.textContent = `Array: ${ myArray }`;
-  
-  // section.appendChild(para1);
+*/
 
 
 
-  
-// Add your code here
 
-let myString = 'Ryu+Ken+Chun-Li+Cammy+Guile+Sakura+Sagat+Juri';
-let myArray = myString.split('+');
-let arrayLength = myArray.length;
-let lastItem = myArray[myArray.length - 1];
 
-// Don't edit the code below here!
 
-section.innerHTML = ' ';
-let para1 = document.createElement('p');
-para1.textContent = `Array: ${ myArray }`;
 
-let para2 = document.createElement('p');
-para2.textContent = `The length of the array is ${ arrayLength }.`;
-
-let para3 = document.createElement('p');
-para3.textContent = `The last item in the array is "${ lastItem }".`;
-
-section.appendChild(para1);
-section.appendChild(para2);
-section.appendChild(para3);
-
-})
+console.log()
