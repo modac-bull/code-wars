4afc01e65b34b292d7beff052e058b34ea9df956
jihoon <ljhljh211@naver.com>
2022-08-30 08:12:43 +0900

    [ADD] 7kyu-rowSumOddNumbers 실습 폴더 추가

---
+++ 7kyu-rowSumOddNumbers/script.js
@@ -0,0 +1,42 @@
+/* 
+
+Given the triangle of consecutive odd numbers:
+
+             1
+          3     5
+       7     9    11
+   13    15    17    19
+21    23    25    27    29
+...
+Calculate the sum of the numbers in the nth row of this triangle (starting at index 1) 
+e.g.: (Input --> Output)
+
+1 -->  1
+2 --> 3 + 5 = 8
+
+
+1행  1
+2행 2 , 3
+3행 4 , 5 , 6
+4행 7, 8, 9, 10
+...
+
+N행 a(n-1) + 2*(n-1)
+
+
+1      2      3       4
+1 (+2) 3 (+4) 7 (+6)  13
+
+
+
+*/
+
+function rowSumOddNumbers(n) {
+	// TODO
+}
+
+
+
+
+
+console.log()
