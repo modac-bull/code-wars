b9fe9c6d77aa599fbb6bf58ff53e3b9f94ff18da
jihoon <ljhljh211@naver.com>
2022-09-16 17:21:04 +0900

    [MOD] 8kyu - squareSum 실습 풀이

--- 8kyu-squareSum/script.js
+++ 8kyu-squareSum/script.js
@@ -1,12 +1,13 @@
 /* 
-설명 : 
 
+Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
 
 */
 
 function squareSum(numbers){
-
-  return  numbers
+  return  numbers.reduce((pv, cv) => pv + Math.pow(cv, 2) , 0)
 }
 
 
