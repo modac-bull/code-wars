93fd93ada0efebfa2c331e5c167759bfe5f435b0
leejihoon <ljhljh211@naver.com>
2022-09-30 08:14:00 +0900

    [MOD] 8kyu-noSpace 실습폴더 추가 및 풀이

---
+++ 8kyu-noSpace/script.js
@@ -0,0 +1,17 @@
+/* 
+설명 : 
+ remove the spaces from the string, then return the resultant string
+
+*/
+
+function noSpace(x){
+  return x.replace(/ /g, "");
+}
+
+
+
+
+console.log(
+  noSpace('8 j 8   mBliB8g  imjB8B8  jl  B'),noSpace('8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd')
+
+)
