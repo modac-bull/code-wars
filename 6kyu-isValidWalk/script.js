a5470742a8c28928c9a1c8fd1b4267d52888eff6
jihoon <ljhljh211@naver.com>
2023-02-28 14:02:18 +0900

    [MOD] 6kyu-isValidWalk 풀이

--- 6kyu-isValidWalk/script.js
+++ 6kyu-isValidWalk/script.js
@@ -17,11 +17,90 @@ Note: you will always receive a valid array containing a random assortment of di
 */
 function isValidWalk(walk) {
   //insert brilliant code here
-
-  return walk
+  let CNT_VTCL = 0;
+  let CNT_HZNTL = 0;
+  console.log(walk);
+  if (walk.length <= 2 || walk.length > 10) return false;
+  walk.map((dir) => {
+    switch (dir) {
+      case "n":
+        CNT_VTCL += 1;
+        break;
+      case "s":
+        CNT_VTCL -= 1;
+        break;
+      case "e":
+        CNT_HZNTL += 1;
+        break;
+      case "w":
+        CNT_HZNTL -= 1;
+        break;
+    }
+  });
+  if (CNT_HZNTL === 0 && CNT_VTCL === 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-console.log(isValidWalk(['n','s','n','s','n','s','n','s','n','s']))
-console.log(isValidWalk(['w','e','w','e','w','e','w','e','w','e','w','e']))
-console.log(isValidWalk(['w']))
-console.log(isValidWalk(['n','n','n','s','n','s','n','s','n','s']))
+console.log(isValidWalk(["n"]));
+console.log(isValidWalk(["n", "s"]));
+console.log(
+  isValidWalk(["w", "e", "w", "e", "w", "e", "w", "e", "w", "e", "w", "e"])
+);
+console.log(isValidWalk(["n", "s", "n", "s", "n", "s", "n", "s", "n", "s"]));
+console.log(
+  isValidWalk(["w", "e", "w", "e", "w", "e", "w", "e", "w", "e", "w", "e"])
+);
+console.log(isValidWalk(["w"]));
+console.log(isValidWalk(["n", "n", "n", "s", "n", "s", "n", "s", "n", "s"]));
+
+/* 
+
+function isValidWalk(walk) {
+  var dx = 0
+  var dy = 0
+  var dt = walk.length
+  
+  for (var i = 0; i < walk.length; i++) {
+    switch (walk[i]) {
+      case 'n': dy--; break
+      case 's': dy++; break
+      case 'w': dx--; break
+      case 'e': dx++; break
+    }
+  }
+  
+  return dt === 10 && dx === 0 && dy === 0
+}
+
+function isValidWalk(walk) {
+  function count(val) {
+    return walk.filter(function(a){return a==val;}).length;
+  }
+  return walk.length==10 && count('n')==count('s') && count('w')==count('e');
+}
+
+function isValidWalk(walk) {
+  const north = walk.filter(item => { return item === "n" }).length;
+  const south = walk.filter(item => { return item === "s" }).length;
+  const east = walk.filter(item => { return item === "e" }).length;
+  const west = walk.filter(item => { return item === "w" }).length;
+  
+  return walk.length === 10 && north === south && east === west;
+}
+
+
+function isValidWalk(walk) {
+  var res = {n:0, w:0, s:0, e:0};
+  walk.forEach((c) => res[c]++);
+  return walk.length === 10 && res.n == res.s && res.e == res.w;
+}
+
+
+function isValidWalk(walk) {
+  return walk.length == 10 && !walk.reduce(function(w,step){ return w + {"n":-1,"s":1,"e":99,"w":-99}[step]},0)
+}
+
+*/
