3e773e8dd7fbf207258b6d1cdb35ad4189483771
jihoon <ljhljh211@naver.com>
2023-03-04 23:01:03 +0900

    [MOD] 7kyu-isIsogram 솔루션 - 참고

--- 7kyu-isIsogram/script.js
+++ 7kyu-isIsogram/script.js
@@ -31,3 +31,47 @@ console.log(isIsogram("aba"));
 console.log(isIsogram("moOse"));
 console.log(isIsogram("isIsogram"));
 console.log(isIsogram(""));
+
+
+/* 
+function isIsogram(str){
+	return new Set(str.toUpperCase()).size == str.length;
+}
+
+function isIsogram(str){ 
+  return !/(\w).*\1/i.test(str)
+}
+
+
+function isIsogram(str){
+  var i, j;
+  str = str.toLowerCase();
+  for(i = 0; i < str.length; ++i)
+    for(j = i + 1; j < str.length; ++j)
+      if(str[i] === str[j])
+        return false;
+  return true;
+}
+
+function isIsogram(str){
+  return !str.match(/([a-z]).*\1/i);
+}
+
+
+function isIsogram(str)  {
+  // Downcase everything, split on letters.
+  str_lc = str.toLowerCase();
+  var letters = str_lc.split('');
+  
+  // Utility: checks whether the given "value" at "index" is actually
+  // first encountered at this "index" position within a string ("self").
+  // Expected to be used with Array#filter.
+  var check4uniqueness = function(value, index, self) { 
+    return self.indexOf(value) === index;
+  }
+  
+  // An isogram string contains no duplicate, hence must have the same
+  // length than the array composed of unique letters from that string.
+  return letters.filter(check4uniqueness).length == str.length;
+}
+*/
