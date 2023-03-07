6cee683582cae7681dae5d8598c3c49101bda7f8
jihoon <ljhljh211@naver.com>
2023-03-07 19:50:02 +0900

    [MOD] 8kyu-invert 설정 및 풀이

---
+++ 8kyu-invert/script.js
@@ -0,0 +1,40 @@
+/* 
+설명 : 
+
+Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+
+invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
+invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
+invert([]) == []
+You can assume that all values are integers. Do not mutate the input array/list.
+
+
+*/
+
+function invert(array) {
+  const resultArr = [];
+  array.map((e) => resultArr.push(e * -1));
+  return resultArr;
+}
+
+console.log(invert([1, 2, 3, 4, 5]));
+console.log(invert([1, -2, 3, -4, 5]));
+console.log(invert([]));
+console.log(invert([0]));
+
+/*  
+const invert = array => array.map(num => -num);
+
+function invert(array) {
+  var newArr = [];
+  for(var i = 0; i < array.length; i++){
+    newArr.push(-array[i]);
+  }
+   return newArr;
+}
+
+function invert(array) {
+   return array.map(e=> e*-1);
+}
+
+*/
