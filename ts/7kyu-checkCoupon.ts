515bd08510c0da1af865595a908628e51fc3832f
jihoon <ljhljh211@naver.com>
2023-03-21 14:04:25 +0900

    [MOD] TS/7kyu-checkCoupon 설정 및 풀이중

---
+++ ts/7kyu-checkCoupon.ts
@@ -0,0 +1,49 @@
+/* 
+설명
+
+Story
+Your online store likes to give out coupons for special occasions. Some customers try to cheat the system by entering invalid codes or using expired coupons.
+
+Task
+Your mission:
+Write a function called checkCoupon which verifies that a coupon code is valid and not expired.
+
+A coupon is no more valid on the day AFTER the expiration date. All dates will be passed as strings in this format: "MONTH DATE, YEAR".
+
+Examples:
+checkCoupon("123", "123", "July 9, 2015", "July 9, 2015")  ===  true
+checkCoupon("123", "123", "July 9, 2015", "July 2, 2015")  ===  false
+
+
+*/
+
+export function checkCoupon(
+  enteredCode: string,
+  correctCode: string,
+  currentDate: string,
+  expirationDate: string
+): boolean {
+  // TODO: code me
+  let isValidCorrect: boolean = false;
+  let isValidExpired: boolean = true;
+  const MONTH_TABLE: string[] = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  // state1
+  if (enteredCode === correctCode) {
+    isValidCorrect = true;
+  }
+
+  return false;
+}
