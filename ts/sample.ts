c82a7c228f5718d11b9cf3656d331738a044f78d
jihoon <ljhljh211@naver.com>
2023-03-26 21:31:07 +0900

    [MOD] TS/8kyu-goals

--- ts/sample.ts
+++ ts/sample.ts
@@ -1,4 +1,25 @@
 /* 
 설명
 
+Messi goals function
+Messi is a soccer player with goals in three leagues:
+
+LaLiga
+Copa del Rey
+Champions
+Complete the function to return his total number of goals in all three leagues.
+
+Note: the input will always be valid.
+
+For example:
+
 */
+
+export function goals(
+  laLigaGoals: number,
+  copaDelReyGoals: number,
+  championsLeagueGoals: number
+): number {
+  //show me the goals!
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}
